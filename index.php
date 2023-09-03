6485a03c7e515c2106a1dcfdfb4b1bfbb338131a
Ignat09 <lilia.lilia.lilia1984@gmail.com>
2023-09-03 19:42:35 +0300

    first_commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>first</title>
+	</head>
+	<body>
+		<h1><?php
+        $a = 5;
+        $b = 4;
+        $c = 3;
+        $d = 2;
+        $ab = $a * $b;
+        $cd = $c * $d;
+        $x = $ab + $cd;
+        echo $x;
+        ?></h1>
+	</body>
+</html>
